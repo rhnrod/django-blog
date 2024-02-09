a12962780c94a00eab42838af865bb88441bddc1
Rhuan <rhnrod@gmail.com>
2024-02-09 10:24:26 -0300

    feat: make post migration

--- mysite/blog/models/post.py
+++ mysite/blog/models/post.py
@@ -8,8 +8,8 @@ STATUS = (
 
 
 class Post(models.Model):
-    title = models.CharField(max_lenght=200, unique=True)
-    slug = models.SlugField(max_lenght=200, unique=True)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
